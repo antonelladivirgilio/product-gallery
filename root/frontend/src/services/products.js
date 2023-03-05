f77b592b199661004d4986df4bab3cfb38bef3c2
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-05 15:46:49 -0300

    feat(frontend): add products service

---
+++ root/frontend/src/services/products.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const getProducts = async({product}) => {
+
+    const apiUrl = `https://api.mercadolibre.com/sites/MLA/search?q=:${product}`;
+    const response = await axios.get(apiUrl).then(res => res.data);
+
+    return response.results;
+}
+
+export const getProductById = async({id}) => {
+
+    const apiUrl = `https://api.mercadolibre.com/items/:${id}`;
+    const response = await axios.get(apiUrl).then(res => res.data);
+    return response;
+}
+
+export const getDescriptionByProductId = async({id}) => {
+
+    const apiUrl = `https://api.mercadolibre.com/items/:${id}/description`;
+    const response = await axios.get(apiUrl).then(res => res.data);
+    return response;
+}

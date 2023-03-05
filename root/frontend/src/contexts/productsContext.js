8735a2b9ed802b0af819c324c602f674f6567a1d
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-05 17:44:02 -0300

    feat(frontend): update ProductsContext to save selected product

--- root/frontend/src/contexts/productsContext.js
+++ root/frontend/src/contexts/productsContext.js
@@ -4,9 +4,10 @@ const ProductsContext = createContext(null);
 
 export const ProductsProvider = ({ children }) => {
     const [products, setProducts] = useState({});
+    const [productSelected, setProductSelected] = useState({});
 
     return (
-        <ProductsContext.Provider value={{ products, setProducts }}>
+        <ProductsContext.Provider value={{ products, setProducts, productSelected, setProductSelected}}>
             {children}
         </ProductsContext.Provider>
     )

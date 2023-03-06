d366fa5f32fab80ceaccc200a2045afb25be2db0
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-06 15:23:44 -0300

    fix: update product controller to use product service and update send json

--- root/backend/controllers/productController.js
+++ root/backend/controllers/productController.js
@@ -1,16 +1,21 @@
-const getAllProducts = (request, response) => {
-    //  Debe consultar el siguiente endpoint: https://api.mercadolibre.com/sites/MLA/search?q=:query
-    const { query } = request.params;
+const productServices = require("../services/productService");
 
-    response.send(`"get all products" ${query}`);
+const getAllProducts = (request, response) => {
+    const { query } = request.params;
+    const allProducts = productServices.getAllProducts(query);
+
+    response.send({
+        status: '200', data: allProducts
+    });
 };
 
 const getProductById = (request, response) => {
     const { id } = request.params;
-    // Debe consultar los siguientes endpoints:
-    // https://api.mercadolibre.com/items/:id
-    // https://api.mercadolibre.com/items/:id​/description
-    response.send(`"get product by id" ${id}`);
+    const productById = productServices.getProductById(id);
+
+    response.send({
+        status: '200', data: productById
+    });
 };
 
 module.exports = {

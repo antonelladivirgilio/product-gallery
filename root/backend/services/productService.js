471a0dd01164e341710bd88a3fae76c781c0924b
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-06 18:46:46 -0300

    feat: call services and install axios

--- root/backend/services/productService.js
+++ root/backend/services/productService.js
@@ -1,17 +1,57 @@
+const { response } = require("express");
+const axios = require('axios');
+
 const getAllProducts = (query) => {
-    const allProducts = [{ hola: query}];
+    const allProducts = [{ hola: query }];
     //  Debe consultar el siguiente endpoint: https://api.mercadolibre.com/sites/MLA/search?q=:query
+
+    try {
+        const callGetAllProducts = async (query) => {
+
+            const apiUrl = `https://api.mercadolibre.com/sites/MLA/search?q=${query}`;
+            const response = await axios.get(apiUrl).then(res => res.data);
+            console.log(`getAllProducts ${response.results}`);
+            return response.results;
+        }
+        callGetAllProducts(query);
+
+    } catch (error) {
+        response
+            .status(error.status || 500)
+            .send({ status: "FAILED", data: { error: error.message || error } })
+    }
 
     return allProducts;
 };
 
 const getProductById = (id) => {
-    const product = [{ product: id }];
-    // Debe consultar los siguientes endpoints:
-    // https://api.mercadolibre.com/items/:id
-    // https://api.mercadolibre.com/items/:id​/description
 
-    return product;
+    const callProductById = async (id) => {
+
+        const apiProductByIdUrl = `https://api.mercadolibre.com/items/${id}`;
+        const apiProductDescriptionUrl = `https://api.mercadolibre.com/items/${id}/description`;
+
+        const promises = [axios.get(apiProductByIdUrl), axios.get(apiProductDescriptionUrl)];
+        try {
+            await Promise.all(promises)
+                .then((results) => {
+                    let product = {};
+
+                    results.map((current, _index) => {
+                        console.log(`getProductById current ${current.data}`);
+                    });
+                })
+        } catch (error) {
+            response
+                .status(error.status || 500)
+                .send({ status: "FAILED", data: { error: error.message || error } })
+        }
+
+        return response;
+    }
+
+    callProductById(id);
+
 };
 
 module.exports = {

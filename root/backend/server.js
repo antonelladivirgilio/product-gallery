67357d668c496282fb3d44b1df5ef09b4723f76a
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-06 14:54:37 -0300

    refactor: change index.js to server.js
    fix: remove .gitkeep
    
    fix: change node scripts to serve server.js

---
+++ root/backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const v1ProductRouter = require("./v1/routes/productRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use("/api", v1ProductRouter);
+
+app.listen(PORT, () => {
+    console.log(`👌 Server running on PORT=${PORT} 👌`);
+});

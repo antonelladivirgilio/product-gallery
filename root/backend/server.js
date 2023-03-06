152e13f65932df24f5ad6a5144eefbf142a2404d
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-06 15:26:08 -0300

    feat: add body parser on server.js

--- root/backend/server.js
+++ root/backend/server.js
@@ -5,6 +5,7 @@ const v1ProductRouter = require("./v1/routes/productRoutes");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use("/api", v1ProductRouter);
 
 app.listen(PORT, () => {

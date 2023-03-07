95ff7157095412fc89081ad7b14413d21b23db60
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-07 15:28:28 -0300

    feat: breadcrumb component functionality

--- root/frontend/src/components/Breadcrumbs/index.js
+++ root/frontend/src/components/Breadcrumbs/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
-export function Breadcrumbs({ ...props }) {
+import { useProducts } from '../../contexts/productsContext';
+
+export function Breadcrumbs({...props }) {
+
+    const { products } = useProducts();
+    const { categories } = products;
+
+    const showBreadcrumbs = categories && categories.length > 0;
 
     return (
-        <Breadcrumb>
-            <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
-            <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
-                Library
-            </Breadcrumb.Item>
-            <Breadcrumb.Item active>Data</Breadcrumb.Item>
-        </Breadcrumb>
+        <>
+            {
+                showBreadcrumbs && (
+                    < Breadcrumb >
+                        {categories.map((category, index) => <Breadcrumb.Item href="#" key={`category_${index}`}>{category}</Breadcrumb.Item>)}
+                    </Breadcrumb >
+                )
+            }
+        </>
+
     )
 }

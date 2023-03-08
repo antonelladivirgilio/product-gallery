c68e8b1f6ff6e8961afe3e84f136f942b3ae027d
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-07 23:49:13 -0300

    styles: product list

--- root/frontend/src/components/ProductList/index.js
+++ root/frontend/src/components/ProductList/index.js
@@ -1,26 +1,28 @@
-import React, { useRef } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { useNavigate } from "react-router-dom";
 
 import { getProductById } from '../../services/products';
 
-import { Row, Col, Card, ListGroup, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup, Image, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { useProducts } from '../../contexts/productsContext';
 import shippingImg from '../../assets/ic_shipping_small.png';
 
 import { isObjectEmpty } from '../../utilities/isObjectEmpty';
+
+import styles from './productList.module.scss';
 
 export function ProductList({ ...props }) {
     const { products, setProductSelected } = useProducts();
     const shippingTooltipImgRef = useRef();
     const navigate = useNavigate();
 
-    const handleProductClick = async (productId) => {
+    const handleProductClick = useCallback(async (productId) => {
         const response = await getProductById({ id: productId });
 
         const { item } = response.data;
         setProductSelected(item);
         navigate(`/items/${productId}`);
-    };
+    }, [products]);
 
     return (
         <>
@@ -28,42 +30,39 @@ export function ProductList({ ...props }) {
                 !isObjectEmpty(products) &&
                 <Row>
                     <Col>
-                        <Card >
-                            <ListGroup variant="flush">
+                        <Card>
+                            <ListGroup variant="flush"  >
                                 {products.items.map((currentProduct, index) => {
                                     const { picture, title, id, price, free_shipping } = currentProduct;
-                                    // const { city_name } = address;
-                               
                                     return (
-                                        index < 4 && <ListGroup.Item key={id}>
-                                            <Card border="light" onClick={() => handleProductClick(id)} style={{ cursor: 'pointer' }}>
-                                                <Row>
-                                                    <Col>
-                                                        <Image rounded src={picture} alt={title} />
-                                                    </Col>
-                                                    <Col lg={8} sm={12}>
-                                                        <Card.Title>
-                                                            $ {price.amount}
-                                                            {
-                                                                free_shipping &&
-                                                                <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Este producto tiene envio gratis</Tooltip>}>
-                                                                    <span className="d-inline-block">
-                                                                        <Image
-                                                                            ref={shippingTooltipImgRef}
-                                                                            roundedCircle
-                                                                            src={shippingImg} />
-                                                                    </span>
-                                                                </OverlayTrigger>
-                                                            }
-                                                        </Card.Title>
-                                                        <Card.Text>
-                                                            {title}
-                                                        </Card.Text>
-                                                    </Col>
-                                                    <Col lg={2} sm={12}>
-                                                        {/* {city_name} */}
-                                                    </Col>
-                                                </Row>
+                                        index < 4 && <ListGroup.Item key={id} bsPrefix={styles.padding_reset}>
+                                            <Card border="light" onClick={() => handleProductClick(id)} className={styles.card_container}>
+                                                <Container fluid bsPrefix={styles.padding_reset}>
+                                                    <Row className={styles.padding_reset}>
+                                                        <Col>
+                                                            <Image className={styles.card_image} src={picture} alt={title} loading="lazy" />
+                                                        </Col>
+                                                        <Col lg={9} sm={12}>
+                                                            <Card.Title>
+                                                                <span className={styles.card_title_price}>$ {price.amount}</span>
+                                                                {
+                                                                    free_shipping &&
+                                                                    <OverlayTrigger overlay={<Tooltip id="tooltip-disabled">Este producto tiene envio gratis</Tooltip>}>
+                                                                        <span className="d-inline-block">
+                                                                            <Image
+                                                                                ref={shippingTooltipImgRef}
+                                                                                roundedCircle
+                                                                                src={shippingImg} />
+                                                                        </span>
+                                                                    </OverlayTrigger>
+                                                                }
+                                                            </Card.Title>
+                                                            <Card.Text bsPrefix={styles.card_description}>
+                                                                {title}
+                                                            </Card.Text>
+                                                        </Col>                                                      
+                                                    </Row>
+                                                </Container>
                                             </Card>
                                         </ListGroup.Item>
                                     )

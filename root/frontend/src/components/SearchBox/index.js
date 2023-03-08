bcbb9c5f7b61da70854093d317ffb392ddd45c31
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-07 22:49:45 -0300

    feat: disable button on call service
    styles: add scss on searchBox
    
    fix: change variables to be a module

--- root/frontend/src/components/SearchBox/index.js
+++ root/frontend/src/components/SearchBox/index.js
@@ -10,9 +10,12 @@ import { getProducts } from '../../services/products';
 import Logo from '../../assets/logo.png';
 import MagnifierSmall from '../../assets/magnifier_small.png';
 
+import styles from './searchBox.module.scss';
+
 export function SearchBox() {
 
     const [inputValue, setInputValue] = useState('');
+    const [isCallingService, setIsCallingService] = useState(false);
     const { _, setProducts } = useProducts();
     const navigate = useNavigate();
 
@@ -25,38 +28,48 @@ export function SearchBox() {
 
         if (inputValue) {
 
-            const response = await getProducts({ product: inputValue });
-            const { categories, items } = response.data;
-            setProducts({ categories, items });
-
-            navigate('/items?search=');
+            setIsCallingService(true);
+            try {
+                const response = await getProducts({ product: inputValue });
+                const { categories, items } = response.data;
+                setProducts({ categories, items });
+                setIsCallingService(false);
+                navigate('/items?search=');
+            } catch (error) {
+                setIsCallingService(false);
+            }
         }
     };
 
     const handleLogoClick = () => navigate('/');
 
     return (
-        <Container>
-            <Row>
-                <Col sm="auto">
-                    <Image onClick={handleLogoClick} rounded src={Logo} alt='Logo de MercadoLibre' loading="lazy" />
-                </Col>
-                <Col>
-                    <Form onSubmit={handleSubmit} >
-                        <InputGroup className="mb-3">
-                            <Form.Control
-                                placeholder="Nunca dejes de buscar"
-                                aria-label="Nunca dejes de buscar"
-                                aria-describedby="button-search"
-                                onChange={handleInputChange}
-                            />
-                            <Button type="submit" variant="secondary" id="button-search">                                
-                                <Image rounded src={MagnifierSmall} alt='lupa' loading="lazy" />
-                            </Button>
-                        </InputGroup>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+        <Row className={styles.container_styles}>
+            <Col>
+                <Container>
+                    <Row>
+                        <Col sm="auto">
+                            <Image onClick={handleLogoClick} rounded src={Logo} alt='Logo de MercadoLibre' loading="lazy" />
+                        </Col>
+                        <Col>
+                            <Form onSubmit={handleSubmit} >
+                                <InputGroup className="mb-3">
+                                    <Form.Control
+                                        placeholder="Nunca dejes de buscar"
+                                        aria-label="Nunca dejes de buscar"
+                                        aria-describedby="button-search"
+                                        onChange={handleInputChange}
+                                        className={styles.input}
+                                    />
+                                    <Button type="submit" disabled={isCallingService} variant="light" id="button-search">
+                                        <Image rounded src={MagnifierSmall} alt='lupa' loading="lazy" />
+                                    </Button>
+                                </InputGroup>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            </Col>
+        </Row>
     );
 }

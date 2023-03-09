a86d2236860e5d8a1490b1ad21a7c3e4de35bc68
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-09 13:03:07 -0300

    feat: ErrorBoundary and NotFound page styles and functionality

--- root/frontend/src/pages/ErrorBoundary/index.js
+++ root/frontend/src/pages/ErrorBoundary/index.js
@@ -1,19 +1,51 @@
+import { useCallback } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
+
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+import styles from './errorBoundary.module.scss';
 
 export function ErrorBoundary() {
     const error = useRouteError();
 
-    if (isRouteErrorResponse(error)) {
+    const navigate = useNavigate();
+
+    const handleGoToHome = useCallback(() => {
+        navigate(`/`);
+    }, [navigate]);
+
+    const HasErrorInfo = useCallback(() => {
         return (
-            <div>
-                <h1>Oops!</h1>
+            <div className={styles.container}>
+                <h1 className={styles.h1}>Oops!</h1>
                 <h2>{error.status}</h2>
                 <p>{error.statusText}</p>
                 {error.data?.message && <p>{error.data.message}</p>}
+                <Button onClick={handleGoToHome}>Ir a la página principal</Button>
             </div>
-        );
-    } else {
-        return <div>Oops</div>;
-    }
+        )
+    }, [error, handleGoToHome]);
+
+    const NoErrorInfo = useCallback(() => {
+        return (
+            <>
+                <div>Oops</div>
+                <Button onClick={handleGoToHome}>Ir a la página principal</Button>
+            </>
+        )
+    }, [handleGoToHome]);
+
+    return (
+        <Container>
+            <Row className="justify-content-md-center">
+                <Col>
+                    {
+                        isRouteErrorResponse(error) ? <HasErrorInfo /> : <NoErrorInfo />
+                    }
+                </Col>
+            </Row>
+        </Container>
+    )
 
 }

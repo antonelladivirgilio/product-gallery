fd146deb443472295adf6013c65d3fb18ef58202
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-08 22:27:09 -0300

    fix: remove props in breadcrumb component

--- root/frontend/src/components/Breadcrumbs/index.js
+++ root/frontend/src/components/Breadcrumbs/index.js
@@ -7,7 +7,7 @@ import breadcrumbChevron from '../../assets/breadcrumb_chevron.png';
 
 import styles from './breadcrumbs.module.scss';
 
-export function Breadcrumbs({ ...props }) {
+export function Breadcrumbs() {
 
     const { products } = useProductsContext();
     const { categories } = products;
@@ -22,7 +22,7 @@ export function Breadcrumbs({ ...props }) {
                     <ol className={styles.container_breadcrumbs}>
                         {
                             categories.map((category, index) => {
-                                
+
                                 return (
                                     <li key={`category_${index}`} className={styles.breadcrumb_item}>
                                         <a href="#">{category}</a>

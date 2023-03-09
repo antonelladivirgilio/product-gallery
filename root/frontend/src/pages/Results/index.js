bd769f22be653c4a33bda3b799e3f349ae617e13
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-03-09 13:04:49 -0300

    feat: set deferred state, add suspense

--- root/frontend/src/pages/Results/index.js
+++ root/frontend/src/pages/Results/index.js
@@ -1,8 +1,8 @@
-import { useCallback, useState, useEffect } from 'react';
+import { useCallback, useState, useEffect, Suspense, useDeferredValue } from 'react';
 import { ProductList } from '../../components/ProductList';
 import { Breadcrumbs } from '../../components/Breadcrumbs';
 
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import { useCategoryContext } from '../../contexts/categoryContext';
 import { getProducts } from '../../services/products';
@@ -11,45 +11,49 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 export function Results() {
 
-    const { state } = useLocation();
+    const { state, search } = useLocation();
     const [products, setProducts] = useState({
         categories: [],
         items: []
     });
+    const deferredProducts = useDeferredValue(products);
 
-    const { categories, setCategories } = useCategoryContext();
+    const { setCategories } = useCategoryContext();
+
+    const navigate = useNavigate();
 
     const getResults = useCallback(async (query) => {
 
         if (query) {
-            // setDisableButton(true);
             try {
                 const { response, cancelGetProducts } = await getProducts({ product: query });
 
                 const { categories, items } = response.data;
                 setProducts({ categories, items });
                 setCategories(categories);
-                // setDisableButton(false);
 
                 return cancelGetProducts.abort();
             } catch (error) {
-                // setDisableButton(false);
+                navigate(`/error`);
             }
         }
-    }, [setCategories]);
+    }, [setCategories, navigate]);
 
     useEffect(() => {
-        getResults(state);
-    }, [getResults, state]);
+        const currentState = state ? state : search.split('=')[1];
+        getResults(currentState);
+    }, [getResults, state, search]);
 
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <Breadcrumbs categories={categories} />
-                </Col>
-            </Row>
-            <ProductList products={products.items} />
-        </Container>
+        <Suspense fallback={<h2>Loading...</h2>}>
+            <Container>
+                <Row>
+                    <Col>
+                        <Breadcrumbs categories={deferredProducts.categories} />
+                    </Col>
+                </Row>
+                <ProductList products={deferredProducts.items} />
+            </Container>
+        </Suspense>
     );
 }
